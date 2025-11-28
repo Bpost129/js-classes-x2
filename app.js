181e4a74e961aef13a15da136d9bdd7e53fd4414
Bpost129 <Bpost129@yahoo.com>
2025-11-27 23:56:25 -0500

    create a Car class and and instance

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+class Car {
+  constructor(make, model) {
+    this.make = make
+    this.model = model
+  }
+}
+
+const myCar = new Car('Ford', 'Bronco')
+console.log(myCar)
+
 const hybridCar = {
   make: 'Toyota',
   model: 'Prius',
@@ -8,3 +18,4 @@ const hybridCar = {
     console.log('Running!')
   },
 }
+
