41768d0034ed110cdbe5309677d967ecaa098f5a
Bpost129 <Bpost129@yahoo.com>
2025-11-27 23:37:56 -0500

    add isRunning attribute and start method to car object

--- app.js
+++ app.js
@@ -2,5 +2,9 @@ const hybridCar = {
   make: 'Toyota',
   model: 'Prius',
   color: 'Black',
-  
+  isRunning: false,
+  start: function() {
+    hybridCar.isRunning = true
+    console.log('Running!')
+  },
 }
