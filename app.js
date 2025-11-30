26bd22abadd2c630cfa6e49bc9f108878919f1f3
Bpost129 <Bpost129@yahoo.com>
2025-11-30 00:13:40 -0500

    add start and stop methods to car class and test

--- app.js
+++ app.js
@@ -5,11 +5,26 @@ class Car {
     this.color = color
     this.isRunning = false
   }
+  start() {
+    this.isRunning = true
+    console.log('Running!')
+  }
+  stop() {
+    this.isRunning = false
+    console.log('Stopped!')
+  }
 }
+
+
 
 const myCar = new Car('Ford', 'Bronco')
 const myDreamCar = new Car('Acura', 'NSX', 'green')
-console.log(myCar)
+// console.log(myCar)
+console.log(myDreamCar)
+
+myDreamCar.start()
+console.log(myDreamCar)
+myDreamCar.stop()
 console.log(myDreamCar)
 
 const hybridCar = {
